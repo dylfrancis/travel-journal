d38885a0a30f9d83f28ff8ea54342afbced0bb3e
Dylan Francis <dylfrancis505@gmail.com>
2022-12-22 13:24:10 -0500

    Add images and data

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    id: "1",
+    title: "Colorado",
+    location: "Breckenridge, CO",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Breckenridge,+CO+80424/@39.5012367,-106.0782304,13z/data=!3m1!4b1!4m5!3m4!1s0x876af63d6c26e7c1:0x628d8f7f6680a27a!8m2!3d39.4816537!4d-106.0383518",
+    date: "July 2021",
+    description:
+      "Breckenridge is a Colorado town at the base of the Rocky Mountains' Tenmile Range. It's known for its ski resort, year-round alpine activities and Gold Rush history. The Victorian core of this former mining town is preserved as the Breckenridge National Historic District, running primarily along Main Street, with colorfully painted buildings from the 1880s and '90s housing shops, galleries and restaurants.",
+    imageUrl: "Winter-Scenic-Low.jpg",
+  },
+  {
+    id: "2",
+    title: "Walt Disney World",
+    location: "Orlando FL",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Walt+Disney+World%C2%AE+Resort/@28.3771857,-81.5729287,17z/data=!3m1!4b1!4m5!3m4!1s0x88dd7ee634caa5f7:0xa71e391fd01cf1a0!8m2!3d28.3771857!4d-81.57074",
+    date: "August 2021",
+    description:
+      "The Walt Disney World Resort, also called Walt Disney World or Disney World, is an entertainment resort complex in Bay Lake and Lake Buena Vista, Florida, United States, near the cities of Orlando and Kissimmee.",
+    imageUrl: "disney-world.jpg",
+  },
+  {
+    id: "3",
+    title: "Cincinnati Zoo",
+    location: "Cincinnati, OH",
+    googleMapsUrl:
+      "https://www.google.com/maps?q=cincinnati+zoo&bih=1001&biw=1728&hl=en&um=1&ie=UTF-8&sa=X&ved=2ahUKEwja1oDn6438AhV1LFkFHX0MBE8Q_AUoAnoECAIQBA",
+    date: "October 2022",
+    description:
+      "The Cincinnati Zoo & Botanical Garden is the sixth oldest zoo in the United States, founded in 1873 and officially opening in 1875. It is located in the Avondale neighborhood of Cincinnati, Ohio.",
+    imageUrl: "cincinnati-zoo.jpg",
+  },
+];
