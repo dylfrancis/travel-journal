63a6d96cc3a8c2dd9cf7e89e77f76d6ce5e63e0f
Dylan Francis <dylfrancis505@gmail.com>
2022-12-22 15:01:55 -0500

    Fix local image issue

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 export const Card = (props) => {
+  let imageUrl = `images/${props.card.imageUrl}`;
   return (
     <div className="card-container">
-      <img src={`../../public/images/${props.card.imgUrl}`} alt="" />
+      <img src={imageUrl} alt="" />
       <div className="info-container">
-        <div className="location"></div>
-        <div className="title"></div>
+        <div className="location">
+          <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon>
+          <div className="location-title">{props.card.location}</div>
+          <div className="google-maps-link">
+            <a href={props.card.googleMapsUrl} target="_blank">
+              View on Google Maps
+            </a>
+          </div>
+        </div>
+        <div className="title">
+          <h1>props.</h1>
+        </div>
         <div className="date"></div>
         <div className="description"></div>
       </div>
