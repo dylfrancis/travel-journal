3be424c3236ae69a120d927302389734f747755f
Dylan Francis <dylfrancis505@gmail.com>
2022-12-24 10:07:38 -0500

    Style body

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,25 +3,33 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 export const Card = (props) => {
+  let location = props.card.location.toUpperCase();
+
   let imageUrl = `images/${props.card.imageUrl}`;
   return (
     <div className="card-container">
-      <img src={imageUrl} alt="" />
+      <img className="card-img" src={imageUrl} alt="" />
       <div className="info-container">
         <div className="location">
-          <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon>
-          <div className="location-title">{props.card.location}</div>
+          <FontAwesomeIcon
+            className="location-icon"
+            icon={faLocationDot}
+          ></FontAwesomeIcon>
+          <div className="location-title">{location}</div>
           <div className="google-maps-link">
-            <a href={props.card.googleMapsUrl} target="_blank">
+            <a href={props.card.googleMapsUrl} rel="noreferrer" target="_blank">
               View on Google Maps
             </a>
           </div>
         </div>
         <div className="title">
-          <h1>props.</h1>
+          <h1>{props.card.title}</h1>
         </div>
         <div className="date"></div>
-        <div className="description"></div>
+        <h4>{props.card.date}</h4>
+        <div className="description">
+          <p>{props.card.description}</p>
+        </div>
       </div>
     </div>
   );
