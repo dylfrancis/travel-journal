f97e35ca7b1d6ad32da9dfa0c069b415d3275b30
Dylan Francis <dylfrancis505@gmail.com>
2022-12-24 11:12:04 -0500

    Add line separator, adjust some styles

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,36 +1,55 @@
 import React from "react";
+import data from "../data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 export const Card = (props) => {
   let location = props.card.location.toUpperCase();
+  let bottomLine;
+  if (parseInt(props.card.id) === data.length) {
+    bottomLine = false;
+  } else {
+    bottomLine = true;
+  }
 
   let imageUrl = `images/${props.card.imageUrl}`;
   return (
-    <div className="card-container">
-      <img className="card-img" src={imageUrl} alt="" />
-      <div className="info-container">
-        <div className="location">
-          <FontAwesomeIcon
-            className="location-icon"
-            icon={faLocationDot}
-          ></FontAwesomeIcon>
-          <div className="location-title">{location}</div>
-          <div className="google-maps-link">
-            <a href={props.card.googleMapsUrl} rel="noreferrer" target="_blank">
-              View on Google Maps
-            </a>
+    <div className="card">
+      <div className="card-container">
+        <img className="card-img" src={imageUrl} alt="" />
+        <div className="info-container">
+          <div className="location">
+            <FontAwesomeIcon
+              className="location-icon"
+              icon={faLocationDot}
+            ></FontAwesomeIcon>
+            <div className="location-title">{location}</div>
+            <div className="google-maps-link">
+              <a
+                href={props.card.googleMapsUrl}
+                rel="noreferrer"
+                target="_blank"
+              >
+                View on Google Maps
+              </a>
+            </div>
+          </div>
+          <div className="title">
+            <h1>{props.card.title}</h1>
+          </div>
+          <div className="date"></div>
+          <h4>{props.card.date}</h4>
+          <div className="description">
+            <p>{props.card.description}</p>
           </div>
         </div>
-        <div className="title">
-          <h1>{props.card.title}</h1>
-        </div>
-        <div className="date"></div>
-        <h4>{props.card.date}</h4>
-        <div className="description">
-          <p>{props.card.description}</p>
-        </div>
+        <div className="horizontal-line"></div>
       </div>
+      {bottomLine && (
+        <div className="horizontal-line">
+          <hr></hr>
+        </div>
+      )}
     </div>
   );
 };
